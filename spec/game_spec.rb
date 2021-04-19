20b66c91aec8df3c01b0d3cf7dbcfd9112915aed
onu henry <onuhenry24@gmail.com>
2021-04-19 15:53:46 +0100

    fix linter

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,35 +1,35 @@
 require './bin/main'
-describe Player do 
-    describe '#name_check' do
+describe Player do
+  describe '#name_check' do
     player = Player.new
-        it " if player's name is blank" do
-            expect(player.name_check("")).to eql(false)
-        end
+    it " if player's name is blank" do
+      expect(player.name_check('')).to eql(false)
     end
+  end
 end
 describe Game do
   describe '#user1_turn' do
     v = gets.chomp
-    it "checks if the moves are between 1-9 on the board" do
+    it 'checks if the moves are between 1-9 on the board' do
       expect(v.to_i.between?(1, 9)).to eq(false)
     end
-    it "checks if the cell has been taken" do
+    it 'checks if the cell has been taken' do
       @player1_choices = []
       @player2_choices = []
-      expect(@player1_choices.include? v.to_i).to eq(false)
-      expect(@player2_choices.include? v.to_i).to eq(false)
+      expect(@player1_choices.include?(v.to_i)).to eq(false)
+      expect(@player2_choices.include?(v.to_i)).to eq(false)
     end
   end
-   describe '#user2_turn' do
+  describe '#user2_turn' do
     v = gets.chomp
-    it "checks if the moves are between 1-9 on the board" do
+    it 'checks if the moves are between 1-9 on the board' do
       expect(v.to_i.between?(1, 9)).to eq(false)
     end
-    it "checks if the cell has been taken" do
+    it 'checks if the cell has been taken' do
       @player1_choices = []
       @player2_choices = []
-      expect(@player1_choices.include? v.to_i).to eq(false)
-      expect(@player2_choices.include? v.to_i).to eq(false)
+      expect(@player1_choices.include?(v.to_i)).to eq(false)
+      expect(@player2_choices.include?(v.to_i)).to eq(false)
     end
   end
 end
@@ -65,6 +65,3 @@ describe Logic do
     end
   end
 end
-
-
-
