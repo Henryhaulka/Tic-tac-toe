30dd7fb201b99541b2cbc2ccd1d11781f7d970a8
onu henry <onuhenry24@gmail.com>
2021-04-19 15:43:58 +0100

    add test for win_combination

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -20,6 +20,34 @@ describe Game do
       expect(@player2_choices.include? v.to_i).to eq(false)
     end
   end
+   describe '#user2_turn' do
+    v = gets.chomp
+    it "checks if the moves are between 1-9 on the board" do
+      expect(v.to_i.between?(1, 9)).to eq(false)
+    end
+    it "checks if the cell has been taken" do
+      @player1_choices = []
+      @player2_choices = []
+      expect(@player1_choices.include? v.to_i).to eq(false)
+      expect(@player2_choices.include? v.to_i).to eq(false)
+    end
+  end
 end
+
+describe Logic do
+  checker = Logic.new
+  describe '#win_combination' do
+    it 'returns an array without elements' do
+      player1_choices = [2, 4, 7, 8]
+      expect(checker.win_combination1(player1_choices)).to eql([])
+    end
+
+    it 'returns an array with matching winning combination' do
+      player1_choices = [1, 3, 4, 2]
+      expect(checker.win_combination1(player1_choices)).to eql([[1, 2, 3]])
+    end
+  end
+end
+
 
 
