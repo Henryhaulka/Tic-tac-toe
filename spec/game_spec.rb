cb010955bcbb580739ca6c771ae66fb503fa733a
Gorgi <gorgimitev@gmail.com>
2021-04-19 15:53:30 +0200

    adding the user1 method to our rspec file

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,3 +7,12 @@ describe Player do
         end
     end
 end
+describe Game do
+  describe '#user1_turn' do
+    v = gets.chomp
+    it "checks if the moves are between 1-9 on the board" do
+      expect(v.to_i.between?(1, 9)).to eql(false)
+    end
+  end
+end
+
